924393fe1c96e738f4abf9df352bfb1deb709858
Sergey <fenrial@gmail.com>
2019-10-08 01:14:42 +0300

    started project

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app.scss'
+import App from './app'
+console.log('Hello from tsx!')
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+)
